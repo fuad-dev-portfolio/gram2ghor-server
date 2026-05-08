dc8db14d98e54b1ee758e41b95c9b3f03e2c9476
fuad-dev-portfolio <abdullahalfuad.dev@gmail.com>
2026-05-08 10:50:49 +0600

    fix: deploy issue

--- src/server.js
+++ src/server.js
@@ -22,7 +22,6 @@ const corsOptions = {
     credentials: true
 }
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
