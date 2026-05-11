c1718aefbeb2e45e7ea23b645bea8d792f0e9677
fuad-dev-portfolio <abdullahalfuad.dev@gmail.com>
2026-05-11 16:09:17 +0600

    fix: category issue

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ import orderRouter from './routes/order.route.js';
 import contactMessageRouter from './routes/contactMessage.route.js';
 import reviewRouter from './routes/review.route.js';
 import clientReviewRouter from './routes/clientReview.route.js';
+import clientCategoryRouter from './routes/clientCategory.route.js';
 import authRouter from './routes/auth.route.js';
 import adminMgmtRouter from './routes/adminMgmt.route.js';
 import authMiddleware from './middlewares/auth.middleware.js';
@@ -51,6 +52,7 @@ app.use("/api/admin/order", authMiddleware, orderRouter);
 app.use("/api/client/contact", contactMessageRouter);
 app.use("/api/admin/review", authMiddleware, reviewRouter);
 app.use("/api/client/review", clientReviewRouter);
+app.use("/api/client/category", clientCategoryRouter);
 app.use("/api/admin/auth", authRouter);
 app.use("/api/admin/admins", authMiddleware, adminMgmtRouter);
 
